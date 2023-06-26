e850a3895f0992d8da02d33ded957b7870e0ebe7
KirillovItStep <kirillov203509@gmail.com>
2023-06-26 21:28:41 +0300

    Pizza-shop

--- src/main/java/org/itstep/pizza/PizzaRepository.java
+++ src/main/java/org/itstep/pizza/PizzaRepository.java
@@ -1,9 +1,6 @@
 package org.itstep.pizza;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-import java.util.List;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Long> {
 }
