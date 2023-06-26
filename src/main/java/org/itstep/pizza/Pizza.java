fa2e6b9d9eba120a009411ae95611abbb348b0dd
KirillovItStep <kirillov203509@gmail.com>
2023-06-26 20:32:47 +0300

    Pizza-shop

---
+++ src/main/java/org/itstep/pizza/Pizza.java
@@ -0,0 +1,16 @@
+package org.itstep.pizza;
+
+@Entity
+@Data
+@Table(name = "pizzas")
+public class Pizza {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String image;
+    private Integer mass;
+    private String desc;
+    private Integer price;
+}
