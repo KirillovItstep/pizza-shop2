00b2fc3f468c033fa9b8a811c661a050b868776a
KirillovItStep <kirillov203509@gmail.com>
2023-06-29 21:05:27 +0300

    Pizza-shop

--- src/main/java/org/itstep/client/ClientService.java
+++ src/main/java/org/itstep/client/ClientService.java
@@ -14,4 +14,8 @@ public class ClientService {
     public List<Client> findAll(){
         return clientRepository.findAll();
     }
+
+    public Client findByLogin(String login){
+        return clientRepository.findByLogin(login).orElse(null);
+    }
 }
--- src/main/java/org/itstep/controller/AppController.java
+++ src/main/java/org/itstep/controller/AppController.java
@@ -2,16 +2,40 @@ package org.itstep.controller;
 
 import org.itstep.client.Client;
 import org.itstep.client.ClientDao;
+import org.itstep.client.ClientService;
+import org.itstep.pizza.PizzaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @Autowired
+    private PizzaService pizzaService;
 
     @GetMapping("/signin")
     public String signin(Model model) {
         model.addAttribute("client", new Client());
         return "signin";
+    }
+
+    @PostMapping("/signin")
+    public String signin(Client client, Model model) {
+        model.addAttribute("client", new Client());
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10);
+        Client clientDb = clientService.findByLogin(client.getLogin()); //Есть ли пользователь в бд
+        if (clientDb==null) return "signin";
+        if (encoder.matches(client.getPassword(), clientDb.getPassword())){
+            model.addAttribute("pizzas", pizzaService.findAll());
+            return "index";
+        }
+        else return "signin";
     }
 }
